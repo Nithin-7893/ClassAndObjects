a8be1a7f8be5aa1aad254db23c2847b8852ba9b3
Badduluri Nithin kumar <nithinkumarnk7893@gmail.com>
2024-12-26 15:26:31 +0530

    Add files via upload

---
+++ Defaultvalues2.java
@@ -0,0 +1,13 @@
+class Nith
+{
+int x;
+String name;
+ static void print()
+{
+System.out.printf("id=%d\nname=%c",x,name);
+}
+public static void main(String args[])
+{
+Constructor.print();
+}
+}								
